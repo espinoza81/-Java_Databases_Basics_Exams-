ef2ac9c1939a24f9a78e1d6f068c231218dfc859
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-29 16:54:44 +0300

    Update 02-11.sql

--- 01 Database Basics MySQL Retake Exam - 10 April 2022/02-11.sql
+++ 01 Database Basics MySQL Retake Exam - 10 April 2022/02-11.sql
@@ -35,3 +35,15 @@ ORDER BY runtime, id
 LIMIT 20;
 
 -- 07. Movie casting
+SELECT 
+	concat_ws(' ', first_name, last_name) AS full_name, 
+	concat(reverse(last_name), length(last_name), '@cast.com') AS email,
+    	2022 - year(birthdate) AS age,
+    	height
+FROM actors
+LEFT JOIN movies_actors AS m 
+ON id = m.actor_id
+WHERE actor_id IS NULL
+ORDER BY height;
+
+-- 08. International festival
