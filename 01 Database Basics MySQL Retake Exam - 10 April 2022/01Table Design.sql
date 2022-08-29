39e710908c740904135675552f45bb8fb83524f0
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-29 16:06:47 +0300

    Update 01Table Design.sql

--- 01 Database Basics MySQL Retake Exam - 10 April 2022/01Table Design.sql
+++ 01 Database Basics MySQL Retake Exam - 10 April 2022/01Table Design.sql
@@ -1,39 +1,39 @@
 CREATE TABLE countries (
- 
+	
 	id INT PRIMARY KEY AUTO_INCREMENT,
-    `name` VARCHAR(30) NOT NULL UNIQUE,
-    continent VARCHAR(30) NOT NULL,
-    currency VARCHAR(5) NOT NULL
+    	`name` VARCHAR(30) NOT NULL UNIQUE,
+    	continent VARCHAR(30) NOT NULL,
+    	currency VARCHAR(5) NOT NULL
 );
 
 CREATE TABLE genres (
  
 	id INT PRIMARY KEY AUTO_INCREMENT,
-    `name` VARCHAR(50) NOT NULL UNIQUE
+    	`name` VARCHAR(50) NOT NULL UNIQUE
 );
 
 
 CREATE TABLE actors (
  
 	id INT PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    birthdate DATE NOT NULL,
+    	first_name VARCHAR(50) NOT NULL,
+    	last_name VARCHAR(50) NOT NULL,
+    	birthdate DATE NOT NULL,
 	height INT,
 	awards INT,
 	country_id INT NOT NULL,
-    CONSTRAINT fk_actor_countrie
-    FOREIGN KEY (country_id)    REFERENCES countries (id)
+    	CONSTRAINT fk_actor_countrie
+    	FOREIGN KEY (country_id)    REFERENCES countries (id)
 );
 
 CREATE TABLE movies_additional_info (
  
 	id INT PRIMARY KEY AUTO_INCREMENT,
-    rating DECIMAL(10, 2) NOT NULL,
-    runtime INT NOT NULL,
-    picture_url VARCHAR(80) NOT NULL,
-    budget DECIMAL(10, 2),
-    release_date DATE NOT NULL,
+    	rating DECIMAL(10, 2) NOT NULL,
+    	runtime INT NOT NULL,
+    	picture_url VARCHAR(80) NOT NULL,
+    	budget DECIMAL(10, 2),
+    	release_date DATE NOT NULL,
 	has_subtitles TINYINT(1),
 	`description` TEXT
 );
@@ -41,23 +41,23 @@ CREATE TABLE movies_additional_info (
 CREATE TABLE movies (
  
 	id INT PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(70) UNIQUE,
+    	title VARCHAR(70) UNIQUE,
 	country_id INT NOT NULL,
 	movie_info_id INT NOT NULL UNIQUE,
-    CONSTRAINT fk_movie_countrie   
+    	CONSTRAINT fk_movie_countrie   
 	FOREIGN KEY (country_id)    REFERENCES countries (id),
-    CONSTRAINT fk_movie_info
-    FOREIGN KEY (movie_info_id)    REFERENCES movies_additional_info (id)
+    	CONSTRAINT fk_movie_info
+    	FOREIGN KEY (movie_info_id)    REFERENCES movies_additional_info (id)
 );
 
 CREATE TABLE movies_actors (
  
 	movie_id INT,
-    actor_id INT,
-    KEY pk_movies_actors(movie_id, actor_id),
-    CONSTRAINT fk_movie  
+    	actor_id INT,
+    	KEY pk_movies_actors(movie_id, actor_id),
+    	CONSTRAINT fk_movie  
 	FOREIGN KEY (movie_id)    REFERENCES movies (id), 
-    CONSTRAINT fk_actor 
+    	CONSTRAINT fk_actor 
 	FOREIGN KEY (actor_id)    REFERENCES actors (id)
 );
 
@@ -65,9 +65,9 @@ CREATE TABLE genres_movies (
  
 	genre_id INT,
 	movie_id INT,
-    KEY pk_genres_movies(genre_id, movie_id),
-    CONSTRAINT fk_movies 
+    	KEY pk_genres_movies(genre_id, movie_id),
+    	CONSTRAINT fk_movies 
 	FOREIGN KEY (movie_id)    REFERENCES movies (id), 
-    CONSTRAINT fk_genre
-    FOREIGN KEY (genre_id) REFERENCES genres (id)
+    	CONSTRAINT fk_genre
+    	FOREIGN KEY (genre_id) REFERENCES genres (id)
 );
