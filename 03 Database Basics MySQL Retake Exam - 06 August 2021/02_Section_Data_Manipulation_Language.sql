16739bbeb72ea8ae4a6978164ca38e176696a4e5
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-30 14:10:53 +0300

    Update 02_Section_Data_Manipulation_Language.sql

--- 03 Database Basics MySQL Retake Exam - 06 August 2021/02_Section_Data_Manipulation_Language.sql
+++ 03 Database Basics MySQL Retake Exam - 06 August 2021/02_Section_Data_Manipulation_Language.sql
@@ -5,3 +5,12 @@ FROM teams
 WHERE id BETWEEN 1 AND 9;
 
 -- 03. Update 
+UPDATE employees AS e
+LEFT JOIN teams AS t
+ON e.id = t.leader_id
+SET e.salary = e.salary + 1000
+WHERE t.leader_id IS NOT NULL 
+	  AND e.salary < 5000
+    AND e.age <= 40;
+    
+-- 04. Delete
