c684971fe0af4779177dd43808b9d7c25ee13ba2
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-30 17:03:57 +0300

    Update 03_Section_Querying.sql

--- 03 Database Basics MySQL Retake Exam - 06 August 2021/03_Section_Querying.sql
+++ 03 Database Basics MySQL Retake Exam - 06 August 2021/03_Section_Querying.sql
@@ -47,3 +47,18 @@ WHERE right(g.`name`, 1) = '2'
 ORDER BY `quarter`;
 
 -- 09. Full info for games
+SELECT g.`name`,
+	IF(g.budget < 50000, 'Normal budget', 'Insufficient budget') AS budget_level,
+    	t.`name`,
+    	a.`name`
+FROM games AS g
+LEFT JOIN games_categories AS gc
+ON gc.game_id = g.id
+JOIN teams AS t
+ON g.team_id = t.id
+JOIN offices AS o
+ON t.office_id = o.id
+JOIN addresses AS a
+ON o.address_id = a.id
+WHERE g.release_date IS NULL AND gc.category_id IS NULL
+ORDER BY g.`name`;
