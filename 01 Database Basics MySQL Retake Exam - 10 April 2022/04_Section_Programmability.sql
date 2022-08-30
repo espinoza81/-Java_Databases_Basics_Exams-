0a876e52d79b97078cbc94d9b2e30f4f6210078f
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-30 09:50:38 +0300

    Update 04_Section_Programmability.sql

--- 01 Database Basics MySQL Retake Exam - 10 April 2022/04_Section_Programmability.sql
+++ 01 Database Basics MySQL Retake Exam - 10 April 2022/04_Section_Programmability.sql
@@ -1,4 +1,23 @@
 -- 10. History movies 
+CREATE FUNCTION udf_actor_history_movies_count(full_name VARCHAR(50))
+RETURNS INT
+DETERMINISTIC
+BEGIN
+	DECLARE history_movies INT;
+    SEt history_movies := (
+		SELECT count(ma.movie_id)
+        FROM movies_actors AS ma
+        JOIN actors AS a
+        ON ma.actor_id = a.id
+        JOIN genres_movies AS gm
+        ON gm.movie_id = ma.movie_id
+        JOIN genres AS g
+        ON gm.genre_id = g.id
+        WHERE concat_ws(' ', a.first_name, a.last_name) = full_name AND g.`name` = 'History'
+        GROUP BY ma.actor_id
+    );
+    RETURN history_movies;
+END
 
 -- 11. Movie awards
 CREATE PROCEDURE udp_award_movie(movie_title VARCHAR(50))
