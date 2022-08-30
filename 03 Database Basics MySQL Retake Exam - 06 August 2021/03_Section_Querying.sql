dd4ca03d7496e11e60177ed1762eb219bdb48a4a
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-30 16:33:17 +0300

    Update 03_Section_Querying.sql

--- 03 Database Basics MySQL Retake Exam - 06 August 2021/03_Section_Querying.sql
+++ 03 Database Basics MySQL Retake Exam - 06 August 2021/03_Section_Querying.sql
@@ -14,3 +14,17 @@ WHERE o.website IS NOT NULL
 ORDER BY t.`name`, a.`name`;
 
 -- 07. Categories Info
+SELECT c.`name`, 
+	count(gc.game_id) AS games_count,
+    round(avg(g.budget), 2) AS avg_budget,
+    max(g.rating) AS max_rating
+FROM games AS g
+JOIN games_categories AS gc
+ON gc.game_id = g.id
+JOIN categories AS c
+ON gc.category_id = c.id
+GROUP BY c.id
+HAVING max_rating >= 9.5
+ORDER BY games_count DESC, c.`name`;
+
+-- 08. Games of 2022
