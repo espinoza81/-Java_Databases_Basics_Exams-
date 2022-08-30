ecc32633387ee50908868264eb72c0f7a3cf1e0a
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-30 13:15:39 +0300

    Update 04_Section_Programmability.sql

--- 02 Database Basics MySQL Exam - 13 February 2022/04_Section_Programmability.sql
+++ 02 Database Basics MySQL Exam - 13 February 2022/04_Section_Programmability.sql
@@ -17,3 +17,13 @@ BEGIN
 END
 
 -- 11. Reduce price
+CREATE PROCEDURE udp_reduce_price(category_name VARCHAR(50))
+BEGIN
+	UPDATE products AS p
+	JOIN categories AS c
+    ON c.id = p.category_id
+    JOIN reviews AS r
+    ON p.review_id = r.id
+    SET p.price = p.price * 0.7
+	WHERE r.rating < 4.00 AND c.`name` = category_name;
+END
