026225ed43d8d7c2245e397e66d5560472257a7c
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-30 11:39:33 +0300

    Update 03_Section_Querying.sql

--- 02 Database Basics MySQL Exam - 13 February 2022/03_Section_Querying.sql
+++ 02 Database Basics MySQL Exam - 13 February 2022/03_Section_Querying.sql
@@ -10,6 +10,11 @@ WHERE price > 1000 AND quantity_in_stock < 30
 ORDER BY quantity_in_stock, id;
 
 -- 07. Review
+SELECT id, content,	rating,	picture_url, published_at 
+FROM reviews
+WHERE left(content, 2) = 'My' AND length(content) > 61
+ORDER BY rating DESC;
 
 -- 08. First customers
+
 -- 09. Best categories
