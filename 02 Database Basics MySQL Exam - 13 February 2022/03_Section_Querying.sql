084f03cdc6b74ef578509277644d13d5de13f4bd
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-30 11:46:42 +0300

    Update 03_Section_Querying.sql

--- 02 Database Basics MySQL Exam - 13 February 2022/03_Section_Querying.sql
+++ 02 Database Basics MySQL Exam - 13 February 2022/03_Section_Querying.sql
@@ -16,5 +16,11 @@ WHERE left(content, 2) = 'My' AND length(content) > 61
 ORDER BY rating DESC;
 
 -- 08. First customers
+SELECT concat_ws(' ', c.first_name, c.last_name) AS full_name, c.address, o.order_datetime 
+FROM customers AS c
+RIGHT JOIN orders AS o
+ON c.id = o.customer_id
+WHERE year(o.order_datetime) <= 2018
+ORDER BY full_name DESC;
 
 -- 09. Best categories
