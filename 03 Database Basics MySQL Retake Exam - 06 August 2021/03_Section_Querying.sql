d04b9c16eec3fd9df78c4eb618ee5da10c8c8866
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-30 16:19:12 +0300

    Update 03_Section_Querying.sql

--- 03 Database Basics MySQL Retake Exam - 06 August 2021/03_Section_Querying.sql
+++ 03 Database Basics MySQL Retake Exam - 06 August 2021/03_Section_Querying.sql
@@ -4,3 +4,13 @@ FROM employees
 ORDER BY salary, id;
 
 -- 06. Addresses of the teams
+SELECT t.`name`, a.`name`, length(a.`name`)
+FROM teams AS t
+JOIN offices AS o
+ON t.office_id = o.id
+JOIN addresses AS a
+ON o.address_id = a.id
+WHERE o.website IS NOT NULL
+ORDER BY t.`name`, a.`name`;
+
+-- 07. Categories Info
