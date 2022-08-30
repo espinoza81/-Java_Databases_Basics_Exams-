60b7081940b7bddf056faf02675f4b03d1081d91
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-30 08:12:12 +0300

    Update and rename 02Section_Data_Manipulation_Language.sql to 02_Section_Data_Manipulation_Language.sql

--- 01 Database Basics MySQL Retake Exam - 10 April 2022/02_Section_Data_Manipulation_Language.sql
+++ 01 Database Basics MySQL Retake Exam - 10 April 2022/02_Section_Data_Manipulation_Language.sql
@@ -1,6 +1,11 @@
 -- 02. Insert 
 INSERT INTO actors (first_name, last_name, birthdate, height, awards, country_id)
-SELECT reverse(first_name), reverse(last_name), date_sub(birthdate, INTERVAL 2 DAY), height + 10, country_id, (SELECT id FROM countries WHERE `name` LIKE 'Armenia')
+SELECT reverse(first_name), 
+	reverse(last_name), 
+	date_sub(birthdate, INTERVAL 2 DAY), 
+	height + 10, 
+	country_id, 
+	(SELECT id FROM countries WHERE `name` LIKE 'Armenia')
     FROM actors
     WHERE id <= 10;
 
