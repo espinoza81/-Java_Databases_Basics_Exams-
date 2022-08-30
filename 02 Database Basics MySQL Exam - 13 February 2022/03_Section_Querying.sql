2400b6c01f74bf730d7d985e1c416463a452f944
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-30 11:34:17 +0300

    Update 03_Section_Querying.sql

--- 02 Database Basics MySQL Exam - 13 February 2022/03_Section_Querying.sql
+++ 02 Database Basics MySQL Exam - 13 February 2022/03_Section_Querying.sql
@@ -4,7 +4,12 @@ FROM categories
 ORDER BY `name` DESC;
 
 -- 06. Quantity
+SELECT id, brand_id, `name`, quantity_in_stock 
+FROM products
+WHERE price > 1000 AND quantity_in_stock < 30
+ORDER BY quantity_in_stock, id;
 
 -- 07. Review
+
 -- 08. First customers
 -- 09. Best categories
