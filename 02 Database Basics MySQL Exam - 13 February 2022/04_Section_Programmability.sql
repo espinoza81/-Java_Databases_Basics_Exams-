d800cb8f3a9912e93b1501f5885ec5a8a7abe1b3
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-30 13:08:31 +0300

    Update 04_Section_Programmability.sql

--- 02 Database Basics MySQL Exam - 13 February 2022/04_Section_Programmability.sql
+++ 02 Database Basics MySQL Exam - 13 February 2022/04_Section_Programmability.sql
@@ -1,3 +1,19 @@
 -- 10. Extract client cards count 
+CREATE FUNCTION udf_customer_products_count(name VARCHAR(30))
+RETURNS INT
+DETERMINISTIC
+BEGIN
+	DECLARE total_products INT;
+    SEt total_products := (
+		SELECT count(op.product_id)
+        FROM orders_products AS op
+        JOIN orders AS o
+        ON op.order_id = o.id
+        JOIN customers AS c
+        ON o.customer_id = c.id
+        WHERE c.first_name = `name`
+    );
+    RETURN total_products;
+END
 
 -- 11. Reduce price
