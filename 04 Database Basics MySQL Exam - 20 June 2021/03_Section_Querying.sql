9a720965dae54d6cb15c5828295a4b29f8c0c086
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-31 13:14:07 +0300

    Update 03_Section_Querying.sql

--- 04 Database Basics MySQL Exam - 20 June 2021/03_Section_Querying.sql
+++ 04 Database Basics MySQL Exam - 20 June 2021/03_Section_Querying.sql
@@ -4,3 +4,13 @@ FROM cars
 ORDER BY id;
 
 -- 06. Drivers and Cars
+SELECT d.first_name, d.last_name, c.make, c.model, c.mileage
+FROM drivers AS d
+JOIN cars_drivers AS cd
+ON d.id = cd.driver_id
+JOIN cars AS c
+ON cd.car_id = c.id
+WHERE c.mileage IS NOT NULL
+ORDER BY c.mileage DESC, d.first_name;
+
+-- 07. Number of courses 
