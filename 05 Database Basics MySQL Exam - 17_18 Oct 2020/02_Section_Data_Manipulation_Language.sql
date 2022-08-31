f42602d25c371ee110e273ec51092c706fca9d3b
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-31 14:55:53 +0300

    Update 02_Section_Data_Manipulation_Language.sql

--- 05 Database Basics MySQL Exam - 17_18 Oct 2020/02_Section_Data_Manipulation_Language.sql
+++ 05 Database Basics MySQL Exam - 17_18 Oct 2020/02_Section_Data_Manipulation_Language.sql
@@ -1,1 +1,10 @@
+-- 02. Insert
+INSERT INTO products_stores
+SELECT p.id, 1
+FROM products AS p
+LEFT JOIN products_stores AS ps 
+ON p.id = ps.product_id
+WHERE ps.store_id IS NULL;
+
+-- 03. Update
 
