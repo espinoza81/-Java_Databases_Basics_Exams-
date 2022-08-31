3b7abb4c1855e65983d7f661191054b84250b778
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-31 11:47:54 +0300

    Update 02_Section_Data_Manipulation_Language.sql

--- 04 Database Basics MySQL Exam - 20 June 2021/02_Section_Data_Manipulation_Language.sql
+++ 04 Database Basics MySQL Exam - 20 June 2021/02_Section_Data_Manipulation_Language.sql
@@ -1,5 +1,8 @@
 -- 02. Insert
-
+INSERT INTO clients (full_name, phone_number)
+SELECT concat_ws(' ', first_name, last_name), concat('(088) 9999', id*2)
+FROM drivers
+WHERE id BETWEEN 10 AND 20;
 
 -- 03. Update 
 
