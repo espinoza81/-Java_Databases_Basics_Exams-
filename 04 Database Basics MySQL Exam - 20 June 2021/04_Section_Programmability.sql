faa832b88f33a82c29baa440296d3b268a4696eb
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-31 14:24:39 +0300

    Update 04_Section_Programmability.sql

--- 04 Database Basics MySQL Exam - 20 June 2021/04_Section_Programmability.sql
+++ 04 Database Basics MySQL Exam - 20 June 2021/04_Section_Programmability.sql
@@ -13,3 +13,23 @@ BEGIN
 END;
 
 -- 11. Full info for address 
+CREATE PROCEDURE udp_courses_by_address (address_name VARCHAR(100))
+BEGIN
+	SELECT a.`name`, c.full_name, 
+		CASE
+			WHEN co.bill <= 20 THEN 'Low'
+			WHEN co.bill <= 30 THEN 'Medium'
+            	ELSE 'High'
+        END AS level_of_bill, ca.make, ca.condition, cat.`name`
+	FROM courses AS co
+	JOIN addresses AS a
+	ON a.id = co.from_address_id
+	LEFT JOIN clients AS c
+	ON co.client_id = c.id
+	LEFT JOIN cars AS ca
+	ON co.car_id = ca.id
+	LEFT JOIN categories AS cat
+	ON ca.category_id = cat.id
+	WHERE a.`name` = address_name
+	ORDER BY ca.make, c.full_name;
+END
