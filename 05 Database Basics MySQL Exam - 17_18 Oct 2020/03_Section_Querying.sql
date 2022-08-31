deca429ad75bed0bed12355933b5dcf9a70109bd
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-31 15:19:59 +0300

    Update 03_Section_Querying.sql

--- 05 Database Basics MySQL Exam - 17_18 Oct 2020/03_Section_Querying.sql
+++ 05 Database Basics MySQL Exam - 17_18 Oct 2020/03_Section_Querying.sql
@@ -4,3 +4,11 @@ FROM employees
 ORDER BY hire_date DESC;
 
 -- 06. Products with old pictures
+SELECT p.`name`, p.price, p.best_before, concat(left(p.`description`, 10), '...') AS short_description, pi.url
+FROM products AS p
+JOIN pictures AS pi
+ON p.picture_id = pi.id
+WHERE length(p.`description`) > 100 AND year(pi.added_on) < 2019 AND p.price > 20
+ORDER BY p.price DESC;
+
+-- 07. Counts of products in stores
