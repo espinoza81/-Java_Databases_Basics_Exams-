a1049238cb06eae3585ddd7bcab8f6eda7aed23e
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-31 15:57:01 +0300

    Update 03_Section_Querying.sql

--- 05 Database Basics MySQL Exam - 17_18 Oct 2020/03_Section_Querying.sql
+++ 05 Database Basics MySQL Exam - 17_18 Oct 2020/03_Section_Querying.sql
@@ -12,3 +12,13 @@ WHERE length(p.`description`) > 100 AND year(pi.added_on) < 2019 AND p.price > 2
 ORDER BY p.price DESC;
 
 -- 07. Counts of products in stores
+SELECT s.`name`, count(ps.product_id) AS product_count,	round(avg(p.price), 2) AS `avg`
+FROM stores AS s
+LEFT JOIN products_stores AS ps
+ON s.id = ps.store_id
+LEFT JOIN products AS p
+ON ps.product_id = p.id
+GROUP BY s.id
+ORDER BY product_count DESC, `avg` DESC, s.id;
+
+-- 08. Specific employee
