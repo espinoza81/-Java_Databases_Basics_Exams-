07624c95234c3ae80b406df0321c70076b86eb59
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-31 11:53:28 +0300

    Update 02_Section_Data_Manipulation_Language.sql

--- 04 Database Basics MySQL Exam - 20 June 2021/02_Section_Data_Manipulation_Language.sql
+++ 04 Database Basics MySQL Exam - 20 June 2021/02_Section_Data_Manipulation_Language.sql
@@ -5,6 +5,8 @@ FROM drivers
 WHERE id BETWEEN 10 AND 20;
 
 -- 03. Update 
-
+UPDATE cars
+SET `condition` = 'C'
+WHERE (mileage >= 800000 OR mileage IS NULL) AND `year` <= 2010 AND make NOT LIKE 'Mercedes-Benz';
     
 -- 04. Delete
