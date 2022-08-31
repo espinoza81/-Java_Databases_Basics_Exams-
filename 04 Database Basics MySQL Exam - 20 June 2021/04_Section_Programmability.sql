a47eba1e05794bc39a7a703a3e86d2019207cb2c
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-31 14:24:18 +0300

    Update 04_Section_Programmability.sql

--- 04 Database Basics MySQL Exam - 20 June 2021/04_Section_Programmability.sql
+++ 04 Database Basics MySQL Exam - 20 June 2021/04_Section_Programmability.sql
@@ -1,3 +1,15 @@
 -- 10. Find all courses by client’s phone number 
+CREATE FUNCTION udf_courses_by_client (phone_num VARCHAR (20)) 
+RETURNS INT
+DETERMINISTIC
+BEGIN
+	DECLARE count INT;
+	SET count := (SELECT count(c.id)
+        	FROM courses AS c 
+        	JOIN clients AS cl 
+        	ON c.client_id = cl.id 
+        	WHERE cl.phone_number = phone_num);
+  	RETURN count;
+END;
 
 -- 11. Full info for address 
