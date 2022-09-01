5dd861a363526c8812b53485ea1ae51daa2fa550
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-09-01 16:08:43 +0300

    Update 02_Section_Data_Manipulation_Language.sql

--- 06 Database Basics MySQL Retake Exam - 31 March 2020/02_Section_Data_Manipulation_Language.sql
+++ 06 Database Basics MySQL Retake Exam - 31 March 2020/02_Section_Data_Manipulation_Language.sql
@@ -5,3 +5,13 @@ FROM users
 WHERE gender = 'M';
 
 -- 03. Update
+UPDATE addresses
+SET country = 
+	CASE
+		WHEN left(country, 1) = 'B' THEN 'Blocked'
+        WHEN left(country, 1) = 'T' THEN 'Test'
+        WHEN left(country, 1) = 'P' THEN 'In Progress'
+        ELSE  country
+	END;
+  
+-- 04. Delete
