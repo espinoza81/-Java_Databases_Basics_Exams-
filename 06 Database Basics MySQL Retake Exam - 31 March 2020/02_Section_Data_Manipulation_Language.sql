6b53103aac136d3edf58c50284c512dd3ac830d4
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-09-01 16:09:13 +0300

    Update 02_Section_Data_Manipulation_Language.sql

--- 06 Database Basics MySQL Retake Exam - 31 March 2020/02_Section_Data_Manipulation_Language.sql
+++ 06 Database Basics MySQL Retake Exam - 31 March 2020/02_Section_Data_Manipulation_Language.sql
@@ -9,9 +9,9 @@ UPDATE addresses
 SET country = 
 	CASE
 		WHEN left(country, 1) = 'B' THEN 'Blocked'
-        WHEN left(country, 1) = 'T' THEN 'Test'
-        WHEN left(country, 1) = 'P' THEN 'In Progress'
-        ELSE  country
+        	WHEN left(country, 1) = 'T' THEN 'Test'
+        	WHEN left(country, 1) = 'P' THEN 'In Progress'
+        	ELSE country
 	END;
   
 -- 04. Delete
