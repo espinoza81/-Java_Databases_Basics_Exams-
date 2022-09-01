cb655d49c20d9eff43d87d6015a41f9a13d4d967
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-09-01 16:35:03 +0300

    Update 03_Section_Querying.sql

--- 06 Database Basics MySQL Retake Exam - 31 March 2020/03_Section_Querying.sql
+++ 06 Database Basics MySQL Retake Exam - 31 March 2020/03_Section_Querying.sql
@@ -4,3 +4,12 @@ FROM users
 ORDER BY age DESC, username;
 
 -- 06. Extract 5 most commented photos
+SELECT p.id, p.`date`, p.`description`, count(c.id) AS commentsCount
+FROM comments AS c
+JOIN photos AS p
+ON c.photo_id = p.id
+GROUP BY c.photo_id
+ORDER BY commentsCount DESC, p.id
+LIMIT 5;
+
+-- 07. Lucky users
