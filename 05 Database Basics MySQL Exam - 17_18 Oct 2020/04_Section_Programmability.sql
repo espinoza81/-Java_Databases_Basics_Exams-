9bafede615ef11b6d0581d01a8c896035673da32
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-09-01 10:26:28 +0300

    Update 04_Section_Programmability.sql

--- 05 Database Basics MySQL Exam - 17_18 Oct 2020/04_Section_Programmability.sql
+++ 05 Database Basics MySQL Exam - 17_18 Oct 2020/04_Section_Programmability.sql
@@ -32,3 +32,15 @@ BEGIN
 END
 
 -- 11. Update product price by address
+CREATE PROCEDURE udp_update_product_price (address_name VARCHAR (50))
+BEGIN
+	UPDATE products AS p
+	JOIN products_stores AS ps
+	ON ps.product_id = p.id
+    JOIN stores AS s
+    ON ps.store_id = s.id
+    JOIN addresses AS a
+    ON s.address_id = a.id
+	SET p.price = IF (left(address_name, 1) = '0', p.price + 100, p.price + 200)
+	WHERE a.`name` = address_name;
+END
