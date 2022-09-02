81e1ea28a330908bc4e0bf9090e5010d5afeb20d
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-09-02 16:32:01 +0300

    Update 04_Section_Programmability.sql

--- 08 Database Basics MySQL Exam - 9 Jun 2019/04_Section_Programmability.sql
+++ 08 Database Basics MySQL Exam - 9 Jun 2019/04_Section_Programmability.sql
@@ -19,8 +19,8 @@ END;
 CREATE PROCEDURE udp_clientinfo (full_name VARCHAR(50))
 BEGIN
 	SELECT c.full_name, c.age, ba.account_number, concat('$', ba.balance) AS balance
-    FROM clients AS c
-	JOIN bank_accounts AS ba
-	ON c.id = ba.client_id
+    	FROM clients AS c
+		JOIN bank_accounts AS ba
+		ON c.id = ba.client_id
 	WHERE c.full_name = full_name;
 END
