31ad6185fbe09c1af33882843f90295ce81c3997
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-09-02 10:06:55 +0300

    Update 04_Section_Programmability.sql

--- 06 Database Basics MySQL Retake Exam - 31 March 2020/04_Section_Programmability.sql
+++ 06 Database Basics MySQL Retake Exam - 31 March 2020/04_Section_Programmability.sql
@@ -14,3 +14,11 @@ BEGIN
 END
 
 -- 11. Increase user age
+CREATE PROCEDURE udp_modify_user (address VARCHAR(30), town VARCHAR(30))
+BEGIN
+	UPDATE users AS u
+		JOIN addresses AS a
+		ON a.user_id = u.id
+	SET u.age = u.age + 10
+	WHERE a.address = address AND a.town = town;
+END
