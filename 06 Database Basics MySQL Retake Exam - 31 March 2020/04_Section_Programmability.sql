0dc239af76d5b63d5ad89c7456235b98a3e7a0df
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-09-02 10:00:51 +0300

    Update 04_Section_Programmability.sql

--- 06 Database Basics MySQL Retake Exam - 31 March 2020/04_Section_Programmability.sql
+++ 06 Database Basics MySQL Retake Exam - 31 March 2020/04_Section_Programmability.sql
@@ -1,3 +1,16 @@
 -- 10. Get user’s photos count 
+CREATE FUNCTION udf_users_photos_count(username VARCHAR(30))
+RETURNS INT
+DETERMINISTIC
+BEGIN
+	DECLARE photosCount INT;
+	SET photosCount := (
+		SELECT count(up.photo_id)
+		FROM users_photos AS up
+		JOIN users AS u
+		ON u.id = up.user_id
+		WHERE u.username = username);
+  	RETURN photosCount;
+END
 
 -- 11. Increase user age
