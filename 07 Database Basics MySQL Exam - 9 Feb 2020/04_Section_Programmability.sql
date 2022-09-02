292a680fafd6b4866f9085f4f197abbfd54121a2
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-09-02 14:04:38 +0300

    Update 04_Section_Programmability.sql

--- 07 Database Basics MySQL Exam - 9 Feb 2020/04_Section_Programmability.sql
+++ 07 Database Basics MySQL Exam - 9 Feb 2020/04_Section_Programmability.sql
@@ -1,3 +1,20 @@
 -- 10. Find all players that play on stadium
+CREATE FUNCTION udf_stadium_players_count (stadium_name VARCHAR(30))
+RETURNS INT
+DETERMINISTIC
+BEGIN
+	DECLARE count INT;
+	
+	SET count := (
+		SELECT count(p.id)
+		FROM stadiums AS s
+		JOIN teams AS t
+		ON t.stadium_id = s.id
+        JOIN players AS p
+        ON p.team_id = t.id
+        WHERE s.`name` = stadium_name);
+        
+  	RETURN count;
+END
 
 -- 11. Find good playmaker by teams 
