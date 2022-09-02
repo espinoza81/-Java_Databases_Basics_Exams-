1cc3412fd96bc51c6df9dc62d20813c5bcc3d32d
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-09-02 14:15:36 +0300

    Update 04_Section_Programmability.sql

--- 07 Database Basics MySQL Exam - 9 Feb 2020/04_Section_Programmability.sql
+++ 07 Database Basics MySQL Exam - 9 Feb 2020/04_Section_Programmability.sql
@@ -18,3 +18,17 @@ BEGIN
 END
 
 -- 11. Find good playmaker by teams 
+CREATE PROCEDURE udp_find_playmaker (min_dribble_points INT, team_name VARCHAR (45))
+BEGIN
+	SELECT concat_ws(' ', first_name, last_name) AS full_name, age, salary, sd.dribbling, sd.speed,	t.`name`
+   	FROM players AS p
+    		JOIN skills_data AS sd
+    		ON p.skills_data_id = sd.id
+    		JOIN teams AS t
+    		ON p.team_id = t.id
+    	WHERE sd.dribbling > min_dribble_points 
+		AND t.`name` = team_name 
+		AND sd.speed > (SELECT avg(speed) FROM skills_data)
+    	ORDER BY sd.speed DESC
+    	LIMIT 1;
+END
