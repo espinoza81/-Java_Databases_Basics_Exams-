bcd5f4f5648c163dc736a9c82b1d71ad52b1c457
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-09-02 10:46:46 +0300

    Update 01_Table_Design.sql

--- 07 Database Basics MySQL Exam - 9 Feb 2020/01_Table_Design.sql
+++ 07 Database Basics MySQL Exam - 9 Feb 2020/01_Table_Design.sql
@@ -1,11 +1,11 @@
 CREATE TABLE skills_data (
 	id INT PRIMARY KEY AUTO_INCREMENT,
-    dribbling INT NOT NULL DEFAULT 0,
-    pace INT NOT NULL DEFAULT 0,
-    passing INT NOT NULL DEFAULT 0,
-    shooting INT NOT NULL DEFAULT 0,
-    speed INT NOT NULL DEFAULT 0,
-    strength INT NOT NULL DEFAULT 0    
+    	dribbling INT NOT NULL DEFAULT 0,
+    	pace INT NOT NULL DEFAULT 0,
+    	passing INT NOT NULL DEFAULT 0,
+    	shooting INT NOT NULL DEFAULT 0,
+    	speed INT NOT NULL DEFAULT 0,
+    	strength INT NOT NULL DEFAULT 0    
 );
 
 CREATE TABLE countries (
@@ -16,67 +16,67 @@ CREATE TABLE countries (
 CREATE TABLE towns (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	`name` VARCHAR(45) NOT NULL,
-    country_id INT NOT NULL,
+    	country_id INT NOT NULL,
 	CONSTRAINT fk_town_country
-    FOREIGN KEY (country_id)
-    REFERENCES countries (id)
+    	FOREIGN KEY (country_id)
+    	REFERENCES countries (id)
 );
 
 CREATE TABLE stadiums (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	`name` VARCHAR(45) NOT NULL,
-    capacity INT NOT NULL,  
-    town_id INT NOT NULL,
+    	capacity INT NOT NULL,  
+    	town_id INT NOT NULL,
 	CONSTRAINT fk_stadium_town
-    FOREIGN KEY (town_id)
-    REFERENCES towns (id)
+    	FOREIGN KEY (town_id)
+    	REFERENCES towns (id)
 );
 
 CREATE TABLE teams (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	`name` VARCHAR(45) NOT NULL,
-    established DATE NOT NULL,
-    fan_base BIGINT NOT NULL DEFAULT 0,  
-    stadium_id INT NOT NULL,
+    	established DATE NOT NULL,
+    	fan_base BIGINT NOT NULL DEFAULT 0,  
+    	stadium_id INT NOT NULL,
 	CONSTRAINT fk_team_stadium
-    FOREIGN KEY (stadium_id)
-    REFERENCES stadiums (id)
+    	FOREIGN KEY (stadium_id)
+    	REFERENCES stadiums (id)
 );
 
 CREATE TABLE players (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	first_name VARCHAR(10) NOT NULL,
 	last_name VARCHAR(20) NOT NULL,
-    age INT NOT NULL DEFAULT 0,
-    position CHAR(1) NOT NULL,
-    salary DECIMAL(10, 2) NOT NULL DEFAULT 0,
-    hire_date DATETIME,
-    skills_data_id INT NOT NULL,
-    team_id INT,
+    	age INT NOT NULL DEFAULT 0,
+    	position CHAR(1) NOT NULL,
+    	salary DECIMAL(10, 2) NOT NULL DEFAULT 0,
+    	hire_date DATETIME,
+    	skills_data_id INT NOT NULL,
+    	team_id INT,
 	CONSTRAINT fk_player_skill
-    FOREIGN KEY (skills_data_id)
-    REFERENCES skills_data (id),
-    CONSTRAINT fk_player_team
-    FOREIGN KEY (team_id)
-    REFERENCES teams (id)
+    	FOREIGN KEY (skills_data_id)
+    	REFERENCES skills_data (id),
+    	CONSTRAINT fk_player_team
+    	FOREIGN KEY (team_id)
+    	REFERENCES teams (id)
 );
 
 CREATE TABLE coaches (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	first_name VARCHAR(10) NOT NULL,
 	last_name VARCHAR(20) NOT NULL,
-    salary DECIMAL(10, 2) NOT NULL DEFAULT 0,
-    coach_level INT NOT NULL DEFAULT 0
+    	salary DECIMAL(10, 2) NOT NULL DEFAULT 0,
+    	coach_level INT NOT NULL DEFAULT 0
 );
 
 CREATE TABLE players_coaches (
 	player_id INT,
 	coach_id INT,
-    KEY pk_players_coaches (player_id, coach_id),
+    	KEY pk_players_coaches (player_id, coach_id),
 	CONSTRAINT fk_player
 	FOREIGN KEY (player_id)    
-    REFERENCES players (id),
+    	REFERENCES players (id),
 	CONSTRAINT fk_coach
 	FOREIGN KEY (coach_id) 
-    REFERENCES coaches (id)
+    	REFERENCES coaches (id)
 );
