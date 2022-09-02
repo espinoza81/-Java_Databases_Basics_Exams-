5330dde37b7c7a177dcf2efb6385dc0cbd693865
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-09-02 12:32:51 +0300

    Update 02_Section_Data_Manipulation_Language.sql

--- 07 Database Basics MySQL Exam - 9 Feb 2020/02_Section_Data_Manipulation_Language.sql
+++ 07 Database Basics MySQL Exam - 9 Feb 2020/02_Section_Data_Manipulation_Language.sql
@@ -12,3 +12,12 @@ SET c.coach_level = c.coach_level + 1
 WHERE left(first_name, 1) = 'A' AND pc.player_id IS NOT NULL;
 
 -- 04. Delete
+-- in Workbench
+DELETE p
+FROM players AS p
+JOIN coaches AS c
+ON c.first_name = p.first_name AND c.last_name = p.last_name;
+
+-- in Judge
+DELETE FROM players
+WHERE age >= 45;
