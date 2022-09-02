ec715257bfcefd97a182ee10803d011a2761bd52
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-09-02 16:04:29 +0300

    Update 03_Section_Querying.sql

--- 08 Database Basics MySQL Exam - 9 Jun 2019/03_Section_Querying.sql
+++ 08 Database Basics MySQL Exam - 9 Jun 2019/03_Section_Querying.sql
@@ -4,3 +4,9 @@ FROM clients
 ORDER BY id;
 
 -- 06. Newbies 
+SELECT id, concat_ws(' ', first_name, last_name) AS full_name, concat('$', salary) AS salary_prefiks, started_on
+FROM employees
+WHERE salary >= 100000 AND started_on >= '2018-01-01'
+ORDER BY salary DESC, id;
+
+-- 07. Cards against Humanity
