6868681f0a6e26e53f006d2535ce88bcf5445efd
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-09-02 10:16:26 +0300

    Update 01_Table_Design.sql

--- 06 Database Basics MySQL Retake Exam - 31 March 2020/01_Table_Design.sql
+++ 06 Database Basics MySQL Retake Exam - 31 March 2020/01_Table_Design.sql
@@ -3,10 +3,10 @@ CREATE TABLE users (
 	username VARCHAR(30) NOT NULL UNIQUE,
 	`password` VARCHAR(30) NOT NULL,
 	email VARCHAR(50) NOT NULL,
-    gender CHAR(1) NOT NULL,
-    age INT NOT NULL,
-    job_title VARCHAR(40) NOT NULL,
-    ip VARCHAR(30) NOT NULL
+    	gender CHAR(1) NOT NULL,
+    	age INT NOT NULL,
+    	job_title VARCHAR(40) NOT NULL,
+    	ip VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE addresses (
@@ -14,10 +14,10 @@ CREATE TABLE addresses (
 	address VARCHAR(30) NOT NULL,
 	town VARCHAR(30) NOT NULL,
 	country VARCHAR(30) NOT NULL,
-    user_id INT NOT NULL,
-    CONSTRAINT fk_address_user
-    FOREIGN KEY (user_id)
-    REFERENCES users (id)
+    	user_id INT NOT NULL,
+    	CONSTRAINT fk_address_user
+    	FOREIGN KEY (user_id)
+    	REFERENCES users (id)
 );
 
 CREATE TABLE photos (
@@ -32,31 +32,31 @@ CREATE TABLE comments (
 	`comment` VARCHAR(255) NOT NULL,
 	`date` DATETIME NOT NULL,
 	photo_id INT NOT NULL,
-    CONSTRAINT fk_comment_photo
-    FOREIGN KEY (photo_id)
-    REFERENCES photos (id)
+    	CONSTRAINT fk_comment_photo
+    	FOREIGN KEY (photo_id)
+    	REFERENCES photos (id)
 );
 
 CREATE TABLE users_photos (
 	user_id INT NOT NULL,
 	photo_id INT NOT NULL,
-    KEY pk_users_photos (user_id, photo_id),
+    	KEY pk_users_photos (user_id, photo_id),
 	CONSTRAINT fk_user
 	FOREIGN KEY (user_id)    
-    REFERENCES users (id),
+    	REFERENCES users (id),
 	CONSTRAINT fk_photo
 	FOREIGN KEY (photo_id) 
-    REFERENCES photos (id)
+    	REFERENCES photos (id)
 );
 
 CREATE TABLE likes (
 	id INT PRIMARY KEY AUTO_INCREMENT,
-    photo_id INT,
+    	photo_id INT,
 	user_id INT,
 	CONSTRAINT fk_like_photo
 	FOREIGN KEY (photo_id) 
-    REFERENCES photos (id),
+    	REFERENCES photos (id),
 	CONSTRAINT fk_like_user
 	FOREIGN KEY (user_id)    
-    REFERENCES users (id)	  
+    	REFERENCES users (id)	  
 );
