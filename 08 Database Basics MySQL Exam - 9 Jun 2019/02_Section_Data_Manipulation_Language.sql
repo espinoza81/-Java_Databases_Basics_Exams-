ece4979b5377ff9087be5ce074bde0a0704f1534
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-09-02 15:58:50 +0300

    Update 02_Section_Data_Manipulation_Language.sql

--- 08 Database Basics MySQL Exam - 9 Jun 2019/02_Section_Data_Manipulation_Language.sql
+++ 08 Database Basics MySQL Exam - 9 Jun 2019/02_Section_Data_Manipulation_Language.sql
@@ -8,11 +8,16 @@ WHERE id BETWEEN 191 AND 200;
 UPDATE employees_clients
 SET employee_id = (SELECT * 
                    FROM (SELECT employee_id 
-					          FROM employees_clients
-                    GROUP BY employee_id
-                    ORDER BY count(client_id), employee_id
-                    LIMIT 1) 
+			FROM employees_clients
+                   	GROUP BY employee_id
+                   	ORDER BY count(client_id), employee_id
+                   	LIMIT 1) 
                    AS s)
 WHERE client_id = employee_id;
 
 -- 04. Delete 
+DELETE e
+FROM employees AS e
+LEFT JOIN employees_clients AS ec
+ON e.id = ec. employee_id
+WHERE ec.client_id IS NULL;
