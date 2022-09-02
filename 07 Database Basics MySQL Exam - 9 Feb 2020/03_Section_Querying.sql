42b342f73cca82d2b0cc7f0f69a3ef4f3cad841a
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-09-02 13:00:28 +0300

    Update 03_Section_Querying.sql

--- 07 Database Basics MySQL Exam - 9 Feb 2020/03_Section_Querying.sql
+++ 07 Database Basics MySQL Exam - 9 Feb 2020/03_Section_Querying.sql
@@ -4,3 +4,11 @@ FROM players
 ORDER BY salary DESC;
 
 -- 06. Young offense players without contract 
+SELECT p.id, concat_ws(' ', first_name, last_name) AS full_name, age, position, hire_date
+FROM players AS p
+JOIN skills_data AS s
+ON p.skills_data_id = s.id
+WHERE age < 23 AND position = 'A' AND hire_date IS NULL AND s.strength > 50
+ORDER BY salary, age;
+
+-- 07. Detail info for all teams 
