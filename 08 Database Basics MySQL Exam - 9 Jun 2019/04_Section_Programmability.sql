cc4e9d3a2bb72cdb863f53cda430a341d53072ac
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-09-02 16:27:36 +0300

    Update 04_Section_Programmability.sql

--- 08 Database Basics MySQL Exam - 9 Jun 2019/04_Section_Programmability.sql
+++ 08 Database Basics MySQL Exam - 9 Jun 2019/04_Section_Programmability.sql
@@ -1,3 +1,18 @@
 -- 10. Extract card's count
+CREATE FUNCTION udf_client_cards_count(`name` VARCHAR(30))
+RETURNS INT
+DETERMINISTIC
+BEGIN
+	DECLARE cards INT;
+	SET cards := (
+		SELECT count(ca.id)
+		FROM clients AS c
+		LEFT JOIN bank_accounts AS ba
+		ON c.id = ba.client_id
+		LEFT JOIN cards AS ca
+		ON ba.id = ca.bank_account_id
+        WHERE c.full_name = `name`);
+  	RETURN cards;
+END;
 
 -- 11. Client Info
