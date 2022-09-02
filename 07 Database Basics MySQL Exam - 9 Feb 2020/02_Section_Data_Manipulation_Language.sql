361e11be9b0ec09d2fd832da4c10aa1f200639bd
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-09-02 10:58:01 +0300

    Update 02_Section_Data_Manipulation_Language.sql

--- 07 Database Basics MySQL Exam - 9 Feb 2020/02_Section_Data_Manipulation_Language.sql
+++ 07 Database Basics MySQL Exam - 9 Feb 2020/02_Section_Data_Manipulation_Language.sql
@@ -1,1 +1,7 @@
 -- 02. Insert 
+INSERT INTO coaches (first_name, last_name, salary, coach_level)
+SELECT first_name, last_name, salary * 2, length(first_name)
+FROM players
+WHERE age >= 45;
+
+-- 03. Update 
