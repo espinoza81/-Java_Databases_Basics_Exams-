5ba26c457fea551d8806decdfbada8212343753b
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-09-03 12:11:08 +0300

    Update 03_Section_Querying.sql

--- 09 Database Basics MySQL Exam - 21 Oct 2018/03_Section_Querying.sql
+++ 09 Database Basics MySQL Exam - 21 Oct 2018/03_Section_Querying.sql
@@ -15,3 +15,11 @@ WHERE purpose = 'Military'
 ORDER BY journey_start;
 
 -- 07. Extract all pilots
+SELECT c.id, concat_ws(' ', c.first_name, c.last_name) AS full_name
+FROM colonists AS c
+JOIN travel_cards AS tc
+ON c.id = tc.colonist_id
+WHERE tc.job_during_journey = 'Pilot'
+ORDER BY c.id;
+
+-- 08. Count all colonists
