226a5da9dc9b85c1de420909344659b8a4d17bce
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-09-04 06:58:04 +0300

    Update 04_Section_Programmability.sql

--- 09 Database Basics MySQL Exam - 21 Oct 2018/04_Section_Programmability.sql
+++ 09 Database Basics MySQL Exam - 21 Oct 2018/04_Section_Programmability.sql
@@ -1,3 +1,22 @@
 -- 15. Get colonists count
+CREATE FUNCTION udf_count_colonists_by_destination_planet (planet_name VARCHAR(30))
+RETURNS INT
+DETERMINISTIC
+BEGIN
+	DECLARE count_colonists INT;
+	SET count_colonists := (
+		SELECT count(c.id)
+		FROM colonists AS c
+		JOIN travel_cards AS tc
+		ON c.id = tc.colonist_id
+		JOIN journeys AS j
+		ON j.id = tc.journey_id
+        JOIN spaceports AS s
+        ON j.destination_spaceport_id = s.id
+        JOIN planets AS p
+        ON p.id = s.planet_id
+        WHERE p.`name` = planet_name);
+  	RETURN count_colonists;
+END
 
 -- 16. Modify spaceship
