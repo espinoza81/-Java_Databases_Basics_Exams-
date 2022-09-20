fdf7018ddfa201ecf70c37b2f5b348ae7ab42835
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-09-20 16:43:41 +0300

    Update 04_Section_Programmability.sql

--- 09 Database Basics MySQL Exam - 21 Oct 2018/04_Section_Programmability.sql
+++ 09 Database Basics MySQL Exam - 21 Oct 2018/04_Section_Programmability.sql
@@ -20,3 +20,14 @@ BEGIN
 END
 
 -- 16. Modify spaceship
+CREATE PROCEDURE udp_modify_spaceship_light_speed_rate(spaceship_name VARCHAR(50), light_speed_rate_increse INT(11))
+BEGIN
+    if (SELECT count(ss.name) FROM spaceships ss WHERE ss.name = spaceship_name > 0) THEN
+        UPDATE spaceships ss
+        SET ss.light_speed_rate = ss.light_speed_rate + light_speed_rate_increse
+        WHERE name = spaceship_name;
+    ELSE
+        SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = 'Spaceship you are trying to modify does not exists.';
+    END IF;
+END;
